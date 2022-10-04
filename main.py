9606449ccfef750cf1bc1fe39fe309eec8ce1b43
MexicanCacti <JosephMRDominguez@gmail.com>
2022-10-04 11:34:00 -0700

    Milo Dominguez

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
+sum = 0
+numbers = [0] * 10
+user_input = int(input())
+numbers[0] = user_input
+max = numbers[0]
+min = numbers[0]
 
-# ******************************
-# Make your Code
-# ******************************
+for i in range(1,5,1):
+    user_input = int(input())
+    if max < user_input:
+        max = user_input
+    if min > user_input:
+        min = user_input
+    numbers[i] = user_input
+
+for i in range(0,5,1):
+    if numbers[i] == min:
+        continue
+    if numbers[i] == max:
+        continue
+    sum += numbers[i]
+print(sum)
